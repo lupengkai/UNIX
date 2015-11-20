70bbef67b740b489d65d65042e86b66186ba127d
tage <763409282@qq.com>
2015-11-20 22:06:16 +0800

    ch1 end

---
+++ 1-8.c
@@ -0,0 +1,12 @@
+#include "apue.h"
+#include <error.h>
+
+int
+main(int argc, char *argv[])
+{
+	fprintf(stderr, "EACCESS: %s\n", strerror(EACCES));
+	errno = ENOENT;
+	perror(argv[0]);
+	exit(0);
+
+}
